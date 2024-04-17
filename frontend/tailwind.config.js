35f74473b084759c2cda0d91f4086fd497ab1253
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-04-17 15:04:37 +0530

    update: configured tailwind to add colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueGray: {
+          '50': '#f8fafc',
+          '100': '#2b335a',
+          // Add more shades as needed
+          '800': '#1c213e',
+        },
+      },
+    },
   },
   plugins: [],
 }
