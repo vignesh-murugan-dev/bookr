801654cee42c5274fd5a5e5c7d294b89e71f4c2d
vignesh-trivecta <vignesh.murugan@trivectadigital.com>
2024-04-17 15:38:51 +0530

    build: created footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center mt-4">Made with ❤️ by&nbsp;<Link target="_blank" to="https://vigneshtech.dev" className="" >Vignesh Murugan</Link></div>
+  )
+}
+
+export default Footer
