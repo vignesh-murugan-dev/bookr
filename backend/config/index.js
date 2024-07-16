878c069674868a605681022b558073c2352d5e64
Vignesh Murugan <vigmurug@gmail.com>
2024-07-16 14:47:56 +0530

    Update index.js

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,7 +3,7 @@ const { seedBooks } = require("../seeders/books-seeder");
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URL || `mongodb+srv://vigmurug:H8HhtCvAdWXo2t0u@cluster0.evpvr6s.mongodb.net/books-review`);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("Connected to DB");
         await seedBooks();
     } catch (error) {
